f9450405cc397678a3d90cb4b46a98a113f118e8
agunbiadepaul <agunbiadepaul94@gmail.com>
2024-07-02 14:49:47 +0100

    complete file

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,17 @@
-const getStudentsByLocation = (students, city) => {
-  const arratyObjec = students.filter((funtion) => funtion.location === city);
-  return arratyObjec;
-};
-
-export default getStudentsByLocation;
+/**
+ * Retrieves students in a given location.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @param {String} city - The location.
+* @author Bezaleel Olakunori <https://github.com/B3zaleel>
+* @returns
+*/
+export default function getStudentsByLocation(students, city) {
+ if (students instanceof Array) {
+   return students.filter((student) => student.location === city);
+ }
+ return [];
+}
